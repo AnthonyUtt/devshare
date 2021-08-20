aee105cf6a9a11c8f8de9892c507293cc254a304
AnthonyUtt <anthony@anthonyutt.dev>
2021-08-19 21:08:06 -0400

    build: set up mobile and web packages

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -6,7 +6,6 @@ SOURCE_DIR=$( dirname "${BASH_SOURCE[0]}" )
 if $SOURCE_DIR/package-check.sh $TRAVIS_COMMIT_RANGE $TARGET; then
     echo "Running job for $TARGET..."
     cd $TARGET
-    yarn install
     yarn run test
 else
     echo "No changes detected for $TARGET."
