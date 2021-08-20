c13b74534ae2441121c60e762cfe3274febfefe2
AnthonyUtt <anthony@anthonyutt.dev>
2021-08-20 09:19:28 -0400

    fix(ci): fix pathing issue in aws setup

--- .travis/aws-setup.sh
+++ .travis/aws-setup.sh
@@ -8,8 +8,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 cp awscliv2.zip $SOURCE_DIR/
 cd $SOURCE_DIR
 unzip awscliv2.zip > /dev/null
-ls -al
-ls -al aws
-$SOURCE_DIR/aws/install --install-dir $SOURCE_DIR --bin-dir $SOURCE_DIR/bin
 
-export AWS=$SOURCE_DIR/bin/aws
+ABS_PATH=$(pwd)
+
+$ABS_PATH/aws/install --install-dir $ABS_PATH --bin-dir $ABS_PATH/bin
+
+export AWS=$ABS_PATH/bin/aws
