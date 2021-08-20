ebceeaf19b204a379eba3ec52c5baaf98b66e13e
AnthonyUtt <anthony@anthonyutt.dev>
2021-08-20 08:58:55 -0400

    ci: fix typo in aws-setup

--- .travis/aws-setup.sh
+++ .travis/aws-setup.sh
@@ -6,7 +6,7 @@ SOURCE_DIR=$( dirname "${BASH_SOURCE[0]}" )
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 cp awscliv2.zip $SOURCE_DIR/
-cd SOURCE_DIR
+cd $SOURCE_DIR
 unzip awscliv2.zip
 $SOURCE_DIR/aws/install --install-dir $SOURCE_DIR --bin-dir $SOURCE_DIR/bin
 
