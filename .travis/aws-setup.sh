85e9a97c944f21896993fb94bb588336c6f4e695
AnthonyUtt <anthony@anthonyutt.dev>
2021-08-20 09:13:13 -0400

    ci: test aws setup issues

--- .travis/aws-setup.sh
+++ .travis/aws-setup.sh
@@ -7,7 +7,9 @@ SOURCE_DIR=$( dirname "${BASH_SOURCE[0]}" )
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 cp awscliv2.zip $SOURCE_DIR/
 cd $SOURCE_DIR
-unzip awscliv2.zip
+unzip awscliv2.zip > /dev/null
+ls -al
+ls -al aws
 $SOURCE_DIR/aws/install --install-dir $SOURCE_DIR --bin-dir $SOURCE_DIR/bin
 
 export AWS=$SOURCE_DIR/bin/aws
