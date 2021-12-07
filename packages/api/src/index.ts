91d03b99d3683bb7536448d11b1b5a8d6d3ad232
AnthonyUtt <anthony@anthonyutt.dev>
2021-12-07 14:53:38 -0500

    refactor(db): migrate from mongo to postgres
    Resolves: N/A
    
    [skip ci]

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { Sequelize } from 'sequelize';
 
 const PORT = process.env.API_PORT || 3030;
 
+const db = process.env.POSTGRES_DB || '';
+const user = process.env.POSTGRES_USER || '';
+const password = process.env.POSTGRES_PASSWORD || '';
+
+const sequelize = new Sequelize(db, user, password, {
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+});
+
 const app = express();
 
-app.get('/', (_, res) => {
+app.get('/', async (_, res) => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to DB established');
+    } catch (error) {
+        console.error('Unable to connect to DB: ', error);
+    }
     res.send('Hello, world!');
 });
 
